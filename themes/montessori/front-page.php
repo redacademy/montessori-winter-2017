725c2113824c37f32ed802f2bb85119152ec4f3b
Tania Amparan <you@email.com>
2017-03-15 13:51:38 -0700

    added button to welcome

--- themes/montessori/front-page.php
+++ themes/montessori/front-page.php
@@ -8,9 +8,18 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-front-page">
 		<main id="main" class="site-main" role="main">
-
+         <div class="hero">
+         <div class="main-picture"> </div><!--main-pic-->
+         <div class="welcome-text">
+         <p>Welcome!</p>
+         <p>The New Westminster Montessori Society supports the public Montessori Programs at
+         Richard McBride and Connaught Heights elementary schools</p>
+         <a href="#" class="myButton">Our Society Role</a>
+         </div><!--welcome-->
+         </div><!--hero-->
+        
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
