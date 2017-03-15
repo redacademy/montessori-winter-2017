e5296d30b40bfe68a8685dcf16b89a9d1eda8d3a
Tania Amparan <you@email.com>
2017-03-15 14:56:55 -0700

    added hero image

--- themes/montessori/front-page.php
+++ themes/montessori/front-page.php
@@ -13,16 +13,23 @@ get_header(); ?>
          <div class="hero">
          <div class="main-picture"> </div><!--main-pic-->
          <div class="welcome-text">
-         <p>Welcome!</p>
-         <p>The New Westminster Montessori Society supports the public Montessori Programs at
+         <p class="welcome">Welcome!</p>
+         <p class="border"></p>
+         <p class="support">The New Westminster Montessori Society supports the public Montessori Programs at
          Richard McBride and Connaught Heights elementary schools</p>
          <a href="#" class="myButton">Our Society Role</a>
          </div><!--welcome-->
          </div><!--hero-->
-        
+         <section class="quick-links">
+         <h2>Quick Links</h2>
+         </section>
+         <section class="we-need">
+         <h2>We Need Your Support</h2>
+         <div class="pie-graph"></div>
+         </section>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php get_template_part( 'template-parts/front-page', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
