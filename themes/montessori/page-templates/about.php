2534f99a25e02c0db134642f50f8a7a1347fbf55
Tania Amparan <you@email.com>
2017-03-16 11:25:28 -0700

    linking pages

--- themes/montessori/page-templates/about.php
+++ themes/montessori/page-templates/about.php
@@ -7,21 +7,23 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-about">
-  <main id="main" class="site-main" role="main">
+    <div id="primary" class="content-about">
+        <main id="main" class="site-main" role="main">
 
-      <section class="about-hero">
-      <header class="entry-header custom-header">
-      <h2 class=""><?php echo the_title();?></h2>
-      </header>
-      </section>
+            <section class="about-hero">
+                <header class="entry-header custom-header">
+                    <h2 class="">
+                        <?php echo the_title();?>
+                    </h2>
+                </header>
+            </section>
 
-      <div class="about-container">
-     <h2>Our Story</h2>
-     <!--<?php echo CFS()->get('about_our_story');?>-->
-     <h2>Our Team</h2>
-     <!--<?php echo CFS()->get('about_our_team');?>-->
-     </div>
-         </main>
-     </div>
- <?php get_footer();?>
+            <div class="about-container">
+
+                <?php echo CFS()->get('title');?>
+                <?php echo CFS()->get('main_text');?>
+                <?php echo CFS()->get('quote_text');?>
+            </div>
+        </main>
+    </div>
+    <?php get_footer();?>
