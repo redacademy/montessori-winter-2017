763dd7efce32c478527383919049546c4bbec52b
Tania Amparan <you@email.com>
2017-03-16 09:44:50 -0700

    added more templates

---
+++ themes/montessori/page-templates/get-involved.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*Template Name: get involved Page
+*
+*@package Inhabitent Theme
+*/
+
+get_header(); ?>
+
+<div id="primary" class="content-about">
+  <main id="main" class="site-main" role="main">
+
+      <section class="about-hero">
+      <header class="entry-header custom-header">
+      <h2 class=""><?php echo the_title();?></h2>
+      </header>
+      </section>
+
+      <div class="about-container">
+     <h2>Our Story</h2>
+     <!--<?php echo CFS()->get('about_our_story');?>-->
+     <h2>Our Team</h2>
+     <!--<?php echo CFS()->get('about_our_team');?>-->
+     </div>
+         </main>
+     </div>
+ <?php get_footer();?>
