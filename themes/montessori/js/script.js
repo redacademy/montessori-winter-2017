b5127736c7f70f39438ac4d88ca897ea53a9e0ad
cyu1188 <cyu1188@gmail.com>
2017-03-29 17:48:21 -0700

    added javascript function to make philosophy box content only appear on click

--- themes/montessori/js/script.js
+++ themes/montessori/js/script.js
@@ -1,6 +1,36 @@
 (function($){
     $(".menu-toggle").on("click", function() {
         $(".main-navigation ul").toggleClass("active");
-        console.log(this)
+    })
+
+    $(".philosophy-blocks1").on("click", function() {
+        $(".philosophy-blocks1").toggleClass("active");
+        $(".block-image1").toggleClass("active");
+        $(".block-content1").toggleClass("active");
+    })
+    $(".philosophy-blocks2").on("click", function() {
+        $(".philosophy-blocks2").toggleClass("active");
+        $(".block-image2").toggleClass("active");
+        $(".block-content2").toggleClass("active");
+    })
+    $(".philosophy-blocks3").on("click", function() {
+        $(".philosophy-blocks3").toggleClass("active");
+        $(".block-image3").toggleClass("active");
+        $(".block-content3").toggleClass("active");
+    })
+    $(".philosophy-blocks4").on("click", function() {
+        $(".philosophy-blocks4").toggleClass("active");
+        $(".block-image4").toggleClass("active");
+        $(".block-content4").toggleClass("active");
+    })
+    $(".philosophy-blocks5").on("click", function() {
+        $(".philosophy-blocks5").toggleClass("active");
+        $(".block-image5").toggleClass("active");
+        $(".block-content5").toggleClass("active");
+    })
+    $(".philosophy-blocks6").on("click", function() {
+        $(".philosophy-blocks6").toggleClass("active");
+        $(".block-image6").toggleClass("active");
+        $(".block-content6").toggleClass("active");
     })
 })(jQuery);
