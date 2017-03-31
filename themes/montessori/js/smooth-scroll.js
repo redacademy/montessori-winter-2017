98eb2459c33fa4f0c4a9ca982e14458b4e54bc7d
Tania Amparan <you@email.com>
2017-03-31 02:13:05 -0700

    made the get involved title buttons change color when clicked

--- themes/montessori/js/smooth-scroll.js
+++ themes/montessori/js/smooth-scroll.js
@@ -12,30 +12,29 @@
       }
     }
   });
+
+
+$(document).ready(function(){
+	
+	$('ul.nav-tabs li a').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.nav-tabs li a').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+});
+
+
+
+
 })(jQuery);
 
 
  
- 		$(document).ready(function() {
-			  $('.nav-tabs > li > a').click(function(event) {
-			    event.preventDefault(); //stop browser to take action for clicked anchor
-			    //get displaying tab content jQuery selector
-			    var active_tab_selector = $('.nav-tabs > li.active > a').attr('href');
-			    //find actived navigation and remove 'active' css
-			    var actived_nav = $('.nav-tabs > li.active');
-			    actived_nav.removeClass('active');
-			    //add 'active' css into clicked navigation
-			    $(this).parents('li').addClass('active');
-			    //hide displaying tab content
-			    $(active_tab_selector).removeClass('active');
-			    $(active_tab_selector).addClass('hide');
-			    //show target tab content
-			    var target_tab_selector = $(this).attr('href');
-			    $(target_tab_selector).removeClass('hide');
-			    $(target_tab_selector).addClass('active');
-			  });
-			});
-
 
 
 
